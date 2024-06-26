cb1e752de9150e71fc9475667a18f1a148b617cd
PrabathBnk <prabathbanukada@gmail.com>
2024-06-26 15:51:47 +0530

    add styles and rounds

--- app.js
+++ app.js
@@ -34,18 +34,37 @@ customer_1.setAge(20);
 console.log(customer_1.age);*/
 
 let randomNumber = Math.floor((Math.random()*10)*1);
+let count = 3;
+let btnAgain = document.getElementById("btnTry");
+btnAgain.hidden = "true";
 
 document.getElementById("btnGuess").addEventListener("click", function(){
+    count--;
     let inputNumber = document.getElementById("inputNumber").value;
-    inputNumber = parseInt(inputNumber);
+    //inputNumber = parseInt(inputNumber);
 
     let result = document.getElementById("lblResult");
 
-    if(inputNumber > randomNumber){
-        result.innerHTML = "Your number greater than the number"
-    }else if(inputNumber < randomNumber){
-        result.innerHTML = "Your number less than the number"
+    if(count != 0){
+        if(inputNumber > randomNumber){
+            result.innerHTML = "Your number greater than the number"
+        }else if(inputNumber < randomNumber){
+            result.innerHTML = "Your number less than the number"
+        }else{
+            btnAgain.value = "Play Again";
+            btnAgain.hidden = "";
+            result.innerHTML = "Your number is correct; You Win!"
+            return;
+        }
     }else{
-        result.innerHTML = "Your number is correct; You Win!"
+        result.innerHTML = "You have no more chances";
+        btnAgain.value = "Try Again";
+        btnAgain.hidden = "";
     }
-})
+    document.getElementById("count").innerHTML = "Rounds: " + count;
+});
+
+btnAgain.addEventListener("click", function(){
+    location.reload();
+});
+
