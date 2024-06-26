e3d4c1986bcea82eb11adcad454973f18959f5f7
PrabathBnk <prabathbanukada@gmail.com>
2024-06-26 14:46:27 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+/*class Customer{
+    name;
+    age;
+    address;
+
+    constructor(name, age, address){
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+    getName(){
+        return this.name;
+    }
+    setName(name){
+        this.name = name;
+    }
+    setAge(age){
+        this.age = age;
+    }
+    setAddress(address){
+        this.address = address;
+    }
+}
+
+let customer_1 = new Customer("Amal", 18, "Colombo");
+
+console.log(customer_1.getName());
+
+customer_1.setName("Kasun");
+console.log(customer_1.getName());
+
+
+customer_1.setAge(20);
+console.log(customer_1.age);*/
+
+
